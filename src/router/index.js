c909990611d15fe438bf429f62967b0d14b85302
Jessica <jessijob@rambler.ru>
2025-06-06 19:11:25 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import FormComponent from '../components/FormComponent.vue';
 import PreviewComponent from '../components/PreviewComponent.vue';
 
-const routes = [
-  { path: '/form', component: FormComponent },
-  { path: '/preview', component: PreviewComponent },
-  { path: '/', redirect: '/form' }
-];
-
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+  history: createWebHistory(process.env.BASE_URL),
+  routes: [
+    { path: '/form', component: FormComponent },
+    { path: '/preview', component: PreviewComponent },
+    { path: '/', redirect: '/form' }
+  ]
 });
 
 export default router;
